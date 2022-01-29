aae735f0c5c5a7d26236143e6c0d09cfe5a96f93
suwa808 <suwa.takeshi.808@gmail.com>
2022-01-29 15:09:35 +0900

    move config

--- sixchan/__init__.py
+++ sixchan/__init__.py
@@ -12,12 +12,7 @@ from markupsafe import Markup, escape
 from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired, Email, Length, Optional
 
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://sixchan:password@localhost:54321/sixchan"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "bazinga"
-
+from sixchan.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
---
+++ sixchan/config.py
@@ -0,0 +1,4 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = "postgresql://sixchan:password@localhost:54321/sixchan"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "bazinga"
