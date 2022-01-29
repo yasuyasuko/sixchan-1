155a658b2aa971d288d5da8ed4529c16c23e65a8
suwa808 <suwa.takeshi.808@gmail.com>
2022-01-29 15:01:14 +0900

    add docker

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
 dev:
 	FLASK_APP=sixchan FLASK_ENV=development flask run
 
+up:
+	docker compose up -d
+
 lint:
 	flake8 --exclude .venv --max-line-length 88
 
 format:
 	black .
 
-.PHONY: dev lint format
+.PHONY: dev up lint format
--- sixchan.py
+++ sixchan.py
@@ -14,7 +14,7 @@ from wtforms.validators import DataRequired, Email, Length, Optional
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///sixchan.db"
+    SQLALCHEMY_DATABASE_URI = "postgresql://sixchan:password@localhost:54321/sixchan"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "bazinga"
 
