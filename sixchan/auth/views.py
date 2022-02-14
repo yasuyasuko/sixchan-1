f752beebc7063d1b3b66c1c41b694da011bf282d
suwa808 <suwa.takeshi.808@gmail.com>
2022-02-14 23:45:45 +0900

    fix html locations

--- sixchan/auth/views.py
+++ sixchan/auth/views.py
@@ -6,10 +6,10 @@ from flask_login import login_user, logout_user
 from sixchan.config import FLASH_LEVEL, FLASH_MESSAGE
 from sixchan.extensions import db
 from sixchan.email import send_email
-from sixchan.forms import LoginForm, SignupForm
+from sixchan.auth.forms import LoginForm, SignupForm
 from sixchan.models import ActivationToken, UserAccount
 
-auth = Blueprint("auth", __name__, url_prefix="me")
+auth = Blueprint("auth", __name__, url_prefix="/auth")
 
 
 @auth.route("/login", methods=["GET", "POST"])
@@ -19,21 +19,21 @@ def login():
         user = UserAccount.query.filter_by(username=form.username.data).first_or_404()
         if not user.activated:
             flash(FLASH_MESSAGE.ACTIVATION_INCOMPLETE, FLASH_LEVEL.ERROR)
-            return redirect(url_for("index"))
+            return redirect(url_for("main.index"))
         if user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
             flash(FLASH_MESSAGE.LOGIN, FLASH_LEVEL.SUCCESS)
-            return redirect(url_for("index"))
+            return redirect(url_for("main.index"))
         else:
             flash(FLASH_MESSAGE.AUTHENTICATION_FAILED, FLASH_LEVEL.ERROR)
-    return render_template("user/login.html", form=form)
+    return render_template("auth/login.html", form=form)
 
 
 @auth.get("/logout")
 def logout():
     logout_user()
     flash(FLASH_MESSAGE.LOGOUT, FLASH_LEVEL.SUCCESS)
-    return redirect(url_for("index"))
+    return redirect(url_for("main.index"))
 
 
 @auth.route("/signup", methods=["GET", "POST"])
@@ -42,10 +42,10 @@ def signup():
     if form.validate_on_submit():
         if UserAccount.query.filter_by(username=form.username.data).first():
             flash(FLASH_MESSAGE.USERNAME_ALREADY_EXISTS, FLASH_LEVEL.ERROR)
-            return render_template("user/signup.html", form=form)
+            return render_template("auth/signup.html", form=form)
         if UserAccount.query.filter_by(email=form.email.data).first():
             flash(FLASH_MESSAGE.EMAIL_ALREADY_EXISTS, FLASH_LEVEL.ERROR)
-            return render_template("user/signup.html", form=form)
+            return render_template("auth/signup.html", form=form)
 
         new_user = UserAccount.signup(
             username=form.username.data,
@@ -64,9 +64,9 @@ def signup():
             ),
         )
         flash(FLASH_MESSAGE.ACTIVATION_LINK_SEND, FLASH_LEVEL.SUCCESS)
-        return redirect(url_for("index"))
+        return redirect(url_for("main.index"))
 
-    return render_template("user/signup.html", form=form)
+    return render_template("auth/signup.html", form=form)
 
 
 @auth.get("/activate/<token_string>")
@@ -74,19 +74,19 @@ def activate(token_string: str):
     token = ActivationToken.query.get(token_string)
     if not token:
         flash(FLASH_MESSAGE.ACTIVATION_TOKEN_INVALID, FLASH_LEVEL.ERROR)
-        return redirect(url_for("index"))
+        return redirect(url_for("main.index"))
     if token.expired:
         flash(FLASH_MESSAGE.ACTIVATION_TOKEN_EXPIRED, FLASH_LEVEL.ERROR)
         # TODO: reissue token?
-        return redirect(url_for("index"))
+        return redirect(url_for("main.index"))
 
     user = UserAccount.query.get(token.user_id)
     if user.activated:
         flash(FLASH_MESSAGE.ACTIVATION_ALREADY_DONE, FLASH_LEVEL.INFO)
-        return redirect(url_for(".login"))
+        return redirect(url_for("auth.login"))
     else:
         user.activate()
         db.session.commit()
         login_user(user)
         flash(FLASH_MESSAGE.ACTIVATION_COMPLETE, FLASH_LEVEL.SUCCESS)
-        return redirect(url_for("index"))
+        return redirect(url_for("main.index"))
