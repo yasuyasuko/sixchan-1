c61590caab127e00f36f3830d1bc71c0d9718331
suwa808 <suwa.takeshi.808@gmail.com>
2022-02-22 07:42:56 +0900

    fix toast

--- sixchan/static/js/base.js
+++ sixchan/static/js/base.js
@@ -10,14 +10,14 @@
         const hiddenFlashMessages = document.getElementById('flash-messages')
         if (hiddenFlashMessages) {
             const flashMessages = hiddenFlashMessages.getElementsByTagName('div')
-            flashMessages.forEach(e => {
-                const message = e.getElementsByName('message')[0].value
-                const background = e.getElementsByName('background')[0].value
+            for (e of flashMessages) {
+                const message = e.getElementsByTagName('input')[0].value
+                const background = e.getElementsByTagName('input')[1].value
                 const options = {
                     style: { main: { background: background } }
                 }
                 iqwerty.toast.toast(message, options)
-            });
+            }
         }
     }
 
